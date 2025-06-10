c3121c874c1202b7e1268a1e8a0a29573d07ce98
manuelgonz <manuel5365.mgl@gmail.com>
2025-06-10 12:00:24 +0200

    a

--- src/data/site-config.ts
+++ src/data/site-config.ts
@@ -39,8 +39,8 @@ export type SiteConfig = {
 };
 
 const siteConfig: SiteConfig = {
-    website: 'https://example.com',
-    title: 'Dante',
+    website: 'https://ManuelGonzalez709.github.io',
+    title: 'Portafolio de Manuel González',
     subtitle: 'Minimal Astro.js theme',
     description: 'Astro.js and Tailwind CSS theme for blog and portfolio by justgoodui.com',
     image: {
